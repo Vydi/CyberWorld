544f674f058a008783b6b4b95b97379f1c6ff40d
name <name@gmail.com>
2022-11-08 14:16:49 +0300

    version 1.0

--- cyber_world/shop/urls.py
+++ cyber_world/shop/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.MainView.as_view(), name='index'),
-    path('products/', views.ProductView.as_view(), name='products'),
-    # path('details', views.DetailView.as_view(), name='details'),
 
 ]
--- cyber_world/shop/views.py
+++ cyber_world/shop/views.py
@@ -8,10 +8,6 @@ class MainView(TemplateView):
     template_name = "index.html"
 
 
-# class DetailView(TemplateView):
-#     template_name = "details.html"
 
-class ProductView(TemplateView):
-    template_name = "product.html"
 
 
